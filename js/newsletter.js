7a741867bb360af9556d0a5caecaaaeceeb2dc97
JakePhillips-Davies <162450362+JakePhillips-Davies@users.noreply.github.com>
2024-05-02 13:08:17 +0100

    dwa

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,21 +1,6 @@
 const newsletterText = document.getElementById(`newsletterTextbox`);
 const newsletterButton = document.getElementById(`newsletterButton`);
 
-fetch(`assets/newsletter.json`).then(function (result) {
-    result.json().then(function (json) {
-        setupNewsletter(json);
-    })
-})
-
-
-function setupNewsletter(json) {
-    console.log(json);
-    newsletterButton.addEventListener(`click`, function (e) {
-        e.preventDefault();
-
-        var email = { email: "" };
-        email.email = newsletterText.value;
-
-        json.push(email);
-    });
-}
+newsletterButton.addEventListener(`click`, function (e) {
+    e.preventDefault();
+});
